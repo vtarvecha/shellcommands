12c77f6282c8a12a9da7fdf7a3b816e85493ec98
Vikas Tarvecha <Vikas.Tarvecha@searshc.com>
2015-04-01 10:39:14 -0500

    Initial
    Commit basic commands

---
+++ shell.sh
@@ -0,0 +1,16 @@
+# NOHUP Usage: STDOUT and STDERR in one file
+sudo -u mapadmin nohup ./TEMP.sh &>TEMP.log &
+
+# DATE PATERNS
+echo `date +%Y-%m-%d:%H:%M:%S`  # 2015-04-01:11:35:12
+echo $(date +"%x %X")			# 04/01/2015 11:37:05 AM
+echo echo `date +%Y%m%d%H%M%S`  # 20150401113825
+
+#CHECK IF HADOOP FILE EXIST
+hadoop fs -test -e /PATH/FILENAME
+inDirChk=$?
+if [ $inDirChk -ne 0 ]; then
+        echo "INFO["`date +%Y-%m-%d:%H:%M:%S`"]: FILE DOESN'T EXIST"
+else
+        echo "INFO ["`date +%Y-%m-%d:%H:%M:%S`"]: FILE EXIT"
+fi
